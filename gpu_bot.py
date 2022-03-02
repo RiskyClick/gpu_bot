06ba6f5c5e73962dc6e8b7ce57fe559d6ede5546
RiskyClick <krich@mail.sfsu.edu>
2022-03-02 13:16:46 -0800

    i think i messed up pretty good here

--- gpu_bot.py
+++ gpu_bot.py
@@ -9,23 +9,20 @@ def send_email(title):
     server = smtplib.SMTP("smtp.gmail.com" , 587)  # 587 = port number
     server.ehlo() # check the smtp connection 
     server.starttls()  # start the conection 
-    server.login("spamingdump@gmail.com" , "s7qzYjBCFBG1")  
-    server.sendmail("spamingdump@gmail.com" , "spamingdump@gmail.com" , title)
+    server.login("log_in_email" , "log_in_pw")  
+    server.sendmail("from_email" , "to_email" , title)
     server.close() 
     
-
 def clickable(title, button):
     if button.is_enabled():
         button.click()
-        #send_email(title)
+        send_email(title)
         exit(0)
-    else:
-        print("Not clickable shoudl refresh now")
 
 while True:
     driver.get("https://www.bestbuy.com/site/nvidia-geforce-rtx-3090-24gb-gddr6x-pci-express-4-0-graphics-card-titanium-and-black/6429434.p?skuId=6429434")
     title = driver.find_element_by_tag_name('h1')
     button = driver.find_element_by_xpath("/html/body/div[3]/main/div[2]/div/div[1]/div[3]/div[2]/div/div[2]/div[1]/div/div/div/button")
     clickable(title.text, button)
-    time.sleep(5)
+    time.sleep(10)
     driver.refresh()
