12a0883a7a4907c8258eb4543023b1fa5d1e3e40
RiskyClick <krich@mail.sfsu.edu>
2022-04-04 17:31:14 -0700

    added a counter and cleaned it up a little.

--- gpu_bot.py
+++ gpu_bot.py
@@ -1,28 +1,26 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import smtplib  # python library to send emails
-import time
 driver = webdriver.Chrome('./chromedriver')
+refresh = 0
 
-
-def send_email(title):
+def send_email():
     server = smtplib.SMTP("smtp.gmail.com" , 587)  # 587 = port number
     server.ehlo() # check the smtp connection 
     server.starttls()  # start the conection 
-    server.login("log_in_email" , "log_in_pw")  
-    server.sendmail("from_email" , "to_email" , title)
-    server.close() 
-    
-def clickable(title, button):
-    if button.is_enabled():
-        button.click()
-        send_email(title)
-        exit(0)
+    server.login("spamingdump@gmail.com" , "&gd8Q48xEl9f")  
+    server.sendmail("spamingdump@gmail.com" , "spamingdump@gmail.com" , "Go Get GPU")
+    server.close()
+    print("email was sent") 
 
 while True:
-    driver.get("https://www.bestbuy.com/site/nvidia-geforce-rtx-3090-24gb-gddr6x-pci-express-4-0-graphics-card-titanium-and-black/6429434.p?skuId=6429434")
-    title = driver.find_element_by_tag_name('h1')
-    button = driver.find_element_by_xpath("/html/body/div[3]/main/div[2]/div/div[1]/div[3]/div[2]/div/div[2]/div[1]/div/div/div/button")
-    clickable(title.text, button)
-    time.sleep(10)
-    driver.refresh()
+    driver.get("https://www.bestbuy.com/site/nvidia-geforce-rtx-3080-10gb-gddr6x-pci-express-4-0-graphics-card-titanium-and-black/6429440.p?skuId=6429440")
+    try:
+        button = driver.find_element_by_xpath("/html/body/div[3]/main/div[2]/div/div[1]/div[3]/div[2]/div/div[2]/div[2]/div/div/div/button")
+        button.click()
+        send_email()
+        break
+    except:
+        refresh += 1
+        driver.refresh()
+        print(f"This bot has refreshed: {refresh} times")
